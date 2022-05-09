5ffe1ff935185c80fa45af2113c7fb9f9c90623e
mehedi hasan <mehedicsebu@gmail.com>
2022-05-09 08:33:29 +0600

    fridge-bd user is connected

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+
 const app = express();
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8c7hq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('fridge-bd connected')
+    // perform actions on the collection object
+    client.close();
+});
+
+
 
 
 app.get('/', (req, res) => {
