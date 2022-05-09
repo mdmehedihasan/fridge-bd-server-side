d4ea712a7dc395e0707509152600f15785dffecf
mehedi hasan <mehedicsebu@gmail.com>
2022-05-09 14:00:26 +0600

    load all data from mongodb

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
-
 const app = express();
 
 //middleware
@@ -12,25 +11,41 @@ app.use(express.json());
 
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8c7hq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('fridge-bd connected')
-    // perform actions on the collection object
-    client.close();
-});
 
 
+
+async function run() {
+    try {
+        await client.connect();
+        const fridgeCollection = client.db('fridgeBD').collection('fridgeItem');
+        app.get('/fridgeItem', async (req, res) => {
+            const query = (0);
+            const cursor = fridgeCollection.find(query);
+            const fridgeItems = await cursor.toArray();
+            res.send(fridgeItems);
+        });
+
+        app.get('/fridgeItem/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const fridgeItem = await fridgeCollection.findOne(query);
+            res.send(fridgeItem);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Fridge Bd Server');
+    res.send('running server');
 });
 
-
 app.listen(port, () => {
-    console.log('listening to port', 5000);
+    console.log('listeing to port');
 })
-
